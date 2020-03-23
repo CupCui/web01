1371bb75a3b6bcdd5f0d5da46a60f3cb274d8997
garre <garrettcui@outlook.com>
2020-03-23 12:10:17 +0800

    Initial commit

---
+++ src/pers/garre/bean/User.java
@@ -0,0 +1,11 @@
+package pers.garre.bean;
+
+/**
+ * @author Garre
+ * @create 2020-03-23 12:01
+ */
+public class User {
+    public static void main(String[] args) {
+        System.out.println("hello world！");
+    }
+}
